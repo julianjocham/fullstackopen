68b7d594ffe5435da7d478fba497d6713e175bb1
JochamJulian <julian.jocham@neteleven.de>
2024-01-23 21:02:43 +0100

    final parts of 1.5 and 1.6-1.14

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    return (
+        <div>
+            <Buttons setGood={setGood} setNeutral={setNeutral} setBad={setBad}/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
+        </div>
+    )
+}
+
+
+const Buttons = ({ setGood, setNeutral, setBad })=>{
+
+    return <div>
+        <h2>give feedback</h2>
+        <button onClick={() => setGood((good) => good +1)}>
+            good
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button onClick={() => setNeutral((neutral) => neutral +1)}>
+            neutral
+        </button>
+        <button onClick={() => setBad((bad) => bad +1)}>
+            bad
+        </button>
+    </div>
+
+}
+
+const Statistics = ({ good, neutral, bad }) =>{
+
+    if (good+neutral+bad === 0){
+        return <div>
+            no feedback given
+        </div>
+    }
+
+    return <div>
+        <h2>statistics</h2>
+        <table>
+            <tbody>
+                <StatisticsLine text={'good'} value={good}/>
+                <StatisticsLine text={'neutral'} value={neutral}/>
+                <StatisticsLine text={'bad'} value={bad}/>
+                <StatisticsLine text={'all'} value={ good + neutral + bad }/>
+                <StatisticsLine text={'average'} value={( good * 1 + bad * -1 ) / ( good + neutral + bad )}/>
+                <StatisticsLine text={'positive'} value={good  / ( good + neutral + bad ) * 100 } />
+            </tbody>
+
+        </table>
+
+    </div>
+
+}
+
+
+const StatisticsLine = ({text,value}) => {
+    return <>
+        <tr>
+            <td>
+                {text}
+            </td>
+            <td>
+            {value} {text === 'positive' && '%'}
+            </td>
+        </tr>
     </>
-  )
 }
 
 export default App
